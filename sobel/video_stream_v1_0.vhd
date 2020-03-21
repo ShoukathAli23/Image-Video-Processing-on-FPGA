31a776e1eaa3f4aefd2199aa5d99afebd3d15b37
shoukath23 <45970703+ShoukathAli23@users.noreply.github.com>
2020-03-21 14:37:55 +0100

    added support to axi dma

--- sobel/video_stream_v1_0.vhd
+++ sobel/video_stream_v1_0.vhd
@@ -32,6 +32,7 @@ entity video_stream_v1_0 is
 		s00_axis_tlast		: in  std_logic;
 		s00_axis_tvalid		: in  std_logic;
         s00_axis_tuser  	: in  std_logic_vector(0 downto 0);
+        s00_axis_tkeep  	: in  std_logic_vector(C_S00_AXIS_TDATA_WIDTH/8 -1 downto 0);
 		-- Ports of Axi Master Bus Interface M00_AXIS
 		m00_axis_aclk		: in  std_logic;
 		m00_axis_aresetn	: in  std_logic;
@@ -39,7 +40,8 @@ entity video_stream_v1_0 is
 		m00_axis_tdata		: out std_logic_vector(C_M00_AXIS_TDATA_WIDTH-1 downto 0);
 		m00_axis_tlast		: out std_logic;
 		m00_axis_tready		: in  std_logic;
-		m00_axis_tuser  	: out std_logic_vector(0 downto 0)
+		m00_axis_tuser  	: out std_logic_vector(0 downto 0);
+		m00_axis_tkeep  	: out  std_logic_vector(C_M00_AXIS_TDATA_WIDTH/8 -1 downto 0)
 	);
 end video_stream_v1_0;
 
@@ -48,7 +50,8 @@ architecture arch_imp of video_stream_v1_0 is
 
 	component RGB2GRAY is
 		generic (
-			data_width 		: integer
+			data_width 		: integer := 24 ;
+			image_width     : integer := 1920
 			);
 		port (	
 			aclk_in 		: in  std_logic;
@@ -57,24 +60,25 @@ architecture arch_imp of video_stream_v1_0 is
 			rgb_tready 		: out std_logic;
 			rgb_tvalid 		: in  std_logic;
 			rgb_tlast 		: in  std_logic;
-			rgb_tuser 		: in  std_logic_vector(s00_axis_tuser'length -1 downto 0);
+--			rgb_tuser 		: in  std_logic_vector(s00_axis_tuser'length -1 downto 0);
 			rgb_tdata 		: in  std_logic_vector(data_width-1 downto 0);
+			rgb_tkeep       : in  std_logic_vector(data_width/8 -1 downto 0);
 			
 			gray_tvalid 	: out std_logic;
 			gray_tlast 		: out std_logic;
-			gray_tuser 		: out std_logic_vector(s00_axis_tuser'length -1 downto 0);
-			gray_tdata 		: out std_logic_vector(data_width/3-1 downto 0);
-				
-			initiate_axis 	: in std_logic;			
+--			gray_tuser 		: out std_logic_vector(s00_axis_tuser'length -1 downto 0);
+			gray_tdata 		: out std_logic_vector(data_width/4-1 downto 0);
+			gray_tready     : in std_logic;	
+            gray_tkeep      : out  std_logic_vector(data_width/8 -1 downto 0);
 			en_conv 		: out std_logic
 			);
 	end component RGB2GRAY;
 
-	component convolution is
+	component intensity_gradient is
 		generic (
-			data_width 		: integer;
-			image_width 	: integer;
-			image_height	: integer
+			data_width 		: integer := 24;
+			image_width 	: integer := 1920;
+			image_height	: integer := 1080
 			);
 		port (	
 			aclk_in 		: in  std_logic;
@@ -84,36 +88,39 @@ architecture arch_imp of video_stream_v1_0 is
 				
 			gray_tvalid 	: in  std_logic;
 			gray_tlast 		: in  std_logic;
-			gray_tuser 		: in  std_logic_vector(s00_axis_tuser'length -1 downto 0);
-			gray_tdata 		: in  std_logic_vector(data_width/3-1 downto 0);
-
+--			gray_tuser 		: in  std_logic_vector(s00_axis_tuser'length -1 downto 0);
+			gray_tdata 		: in  std_logic_vector(data_width/4-1 downto 0);
+            gray_tready     : out std_logic;
+            gray_tkeep      : in  std_logic_vector(data_width/8 -1 downto 0);
+            
 			aclk_out		: in  std_logic;
 			aresetn_out		: in  std_logic;
-				
-			initiate_axis 	: out std_logic;
 				
 			conv_tready 	: in  std_logic;
 			conv_tvalid 	: out std_logic;
 			conv_tlast 		: out std_logic;
-			conv_tuser 		: out std_logic_vector(s00_axis_tuser'length -1 downto 0);
+--			conv_tuser 		: out std_logic_vector(s00_axis_tuser'length -1 downto 0);
+			conv_tkeep      : out  std_logic_vector(data_width/8 -1 downto 0);
 			conv_tdata 		: out std_logic_vector(data_width-1 downto 0)
 			);
-	end component convolution;
+	end component intensity_gradient;
 
 
 	signal tvalid, g_tvalid	: std_logic;
 	signal tlast, g_tlast  	: std_logic;
-	signal tuser, g_tuser  	: std_logic_vector(s00_axis_tuser'length -1 downto 0);
-	signal g_tdata      	: std_logic_vector(C_S00_AXIS_TDATA_WIDTH/3 -1 downto 0);
-	signal init_axis, en   	: std_logic;
-    signal tready           : std_logic;
+--	signal tuser, g_tuser  	: std_logic_vector(s00_axis_tuser'length -1 downto 0);
+	signal g_tdata      	: std_logic_vector(C_S00_AXIS_TDATA_WIDTH/4 -1 downto 0);
+	signal en             	: std_logic;
+    signal tready, g_tready : std_logic;
     signal tdata            : std_logic_vector(C_M00_AXIS_TDATA_WIDTH-1 downto 0);
-
+    signal tkeep, g_tkeep   : std_logic_vector(C_M00_AXIS_TDATA_WIDTH/8 -1 downto 0);
+    
 begin
 
 	stage1: RGB2GRAY 
 		generic map (
-			data_width 		=> C_S00_AXIS_TDATA_WIDTH
+			data_width 		=> C_S00_AXIS_TDATA_WIDTH,
+			image_width     => image_width
 			)
 		port map (	
 			aclk_in 		=> s00_axis_aclk,
@@ -122,20 +129,21 @@ begin
 			rgb_tready 		=> s00_axis_tready,
 			rgb_tvalid 		=> s00_axis_tvalid,
 			rgb_tlast 		=> s00_axis_tlast,
-			rgb_tuser 		=> s00_axis_tuser,
+--			rgb_tuser 		=> s00_axis_tuser,
 			rgb_tdata 		=> s00_axis_tdata,
+		    rgb_tkeep       => s00_axis_tkeep,
 			
 			gray_tvalid 	=> g_tvalid,
 			gray_tlast 		=> g_tlast,
-			gray_tuser 		=> g_tuser,
+--			gray_tuser 		=> g_tuser,
 			gray_tdata 		=> g_tdata,
-				
-			initiate_axis 	=> init_axis,			
+			gray_tready     => g_tready,	
+		    gray_tkeep      => g_tkeep,
 			en_conv 		=> en 
 			);
 
 
-	stage2: convolution
+	stage2: intensity_gradient
 		generic map (
 			data_width 		=> C_M00_AXIS_TDATA_WIDTH,
 			image_width 	=> image_width,
@@ -149,27 +157,38 @@ begin
 			
 			gray_tvalid 	=> g_tvalid,
 			gray_tlast 		=> g_tlast,
-			gray_tuser 		=> g_tuser,
+--			gray_tuser 		=> g_tuser,
 			gray_tdata 		=> g_tdata,
-
+            gray_tready     => g_tready,
+            gray_tkeep      => g_tkeep,
 			aclk_out		=> m00_axis_aclk,
 			aresetn_out		=> m00_axis_aresetn,
-				
-			initiate_axis 	=> init_axis,
 				
 			conv_tready 	=> tready,
 			conv_tvalid 	=> tvalid,
 			conv_tlast 		=> tlast,
-			conv_tuser 		=> tuser,
+--			conv_tuser 		=> tuser,
+			conv_tkeep      => tkeep,
 			conv_tdata 		=> tdata	
 			);
-
-	m00_axis_tvalid 	<= tvalid;
-	m00_axis_tlast  	<= tlast;
-	m00_axis_tuser  	<= tuser;
-	m00_axis_tdata  	<= tdata;
-	
-	tready          	<= m00_axis_tready;
+process(m00_axis_aclk)
+begin
+if rising_edge(m00_axis_aclk) then
+    if m00_axis_aresetn = '1' then
+        m00_axis_tvalid 	<= g_tvalid;
+        m00_axis_tlast  	<= g_tlast;
+--        m00_axis_tuser  	<= g_tuser;
+        m00_axis_tkeep      <= tkeep;
+        m00_axis_tdata      <= tdata;
+--        m00_axis_tdata(23 downto 16)  	<= tdata;
+--        m00_axis_tdata(15 downto  8)  	<= tdata;
+--        m00_axis_tdata( 7 downto  0)  	<= tdata;
+--        m00_axis_tdata(31 downto 24)    <= (others => '0');
+    end if;
+ end if;	
+end process;	
+	g_tready          	<= m00_axis_tready;
 	
 
 end arch_imp;
+
